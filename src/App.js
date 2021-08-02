cadc46214be25151d8f53e10f80d7c9030ec30ce
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-02 11:18:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {
+  return(
+    <header>
+      <h1>{props.name}'s restaurant</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return(
+    <section>
+      <p>We got dat {props.adjective} food</p>
+      <ul style = {{textAlign: "left"}}>
+        {props.dishes.map((dish) => ( //use parens when returning an obj from a map func
+          <li key = {dish.id}>{dish.title}</li>
+          ))}
+      </ul>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return(
+    <footer>
+      Copyright {props.year}
+    </footer>
+  )
+}
+
+
+const dishes = [
+  "Halibut",
+  "Spam and Eggs",
+  "Creamed Cum",
+  "Hot and Bothered Bagel"
+];
+
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }))
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Colyn" />
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
