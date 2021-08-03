3c979737ea8ee8069c5916292d910f0f1b685a82
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-03 09:58:04 -0500

    finished array destructuring vid

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'; 
 
+//array destructuring. 
+const [, , light] = ["boots", "tent", "headlamp"];
+console.log(light);
 
 ReactDOM.render(
   <App  authorized = {true} />,
