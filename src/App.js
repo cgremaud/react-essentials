a3e4ae7892ddf64801555cdeb93d128b202b78fb
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-03 10:29:55 -0500

    finished useEffect video

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired")
   
+  useEffect(() => {
+    console.log(`It's ${emotion} around here.`)
+  }, [emotion])
+
+  useEffect(() => {
+    console.log(`It's ${secondary} too!`)
+  }, [secondary])
+
   return (
     <>
-      <h1>Current emotion: {emotion}</h1>
+      <h1>Current emotion: {emotion}, and {secondary}</h1>
       <button onClick={ () => setEmotion("frustrated") }>
         Frustrate
       </button>
       <button onClick = { () => setEmotion("horny") }>
         Hornify
+      </button>
+      <button onClick = { () => setSecondary("energized") }>
+        Secondary: Energized
+      </button>
+      <button onClick = { () => setSecondary("crabby") }>
+        Secondary: Crabby
       </button>
     </>
   );
